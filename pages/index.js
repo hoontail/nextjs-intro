bc60e154570f615020129943bcbd613bb979e1e5
hoontail <sper456@naver.com>
2022-06-07 18:54:01 +0900

    최상위 컴포_app 이름은 고정

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,15 @@ export default function Home(){
     <div>
       <NavBar/>
       <h1>hello</h1>
-     
+      {/* 글로벌만 추가해주면 전역 (최상위여서 그런가 ?) */}
+      <style jsx global>  
+        {`
+          a{
+            color:white
+          }
+        `}
+
+      </style>
 
     </div>
 
