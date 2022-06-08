c3fbd896ac56ea4fe6b13893154deacb0586d3e8
hoontail <sper456@naver.com>
2022-06-08 22:44:42 +0900

    검색엔진 노출 연습해보기

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,11 @@
 
-import Head from "next/head";
+
 
 export default function Layout({ children }) {
   return (
     <>
 
-      <Head>
-        <title>hoontail`s Portfolio</title>
-      </Head>
+
       <div>{children}</div>
     </>
   );
