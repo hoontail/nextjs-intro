b2aeffd8cb70d845c878c8b342adb73f5814bba9
hoontail <sper456@naver.com>
2022-06-09 10:08:09 +0900

    오타로 인한 실패. 수정 테스트

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  imagae: {
+  images: {
     loader: 'imgix',
     path: "https://example.com/myaccount/",
   },
