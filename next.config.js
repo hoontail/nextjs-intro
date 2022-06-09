60ef5d7ee3607b523ffaa8421c40de89c55785f4
hoontail <sper456@naver.com>
2022-06-09 10:06:19 +0900

    이미지 자동배포 실패로 인한 수정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  imagae: {
+    loader: 'imgix',
+    path: "https://example.com/myaccount/",
+  },
 }
 
 module.exports = nextConfig
