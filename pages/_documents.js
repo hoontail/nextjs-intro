df539ddf105a824fde6e1fe74134a08788949ba7
hoontail <sper456@naver.com>
2022-06-14 16:22:42 +0900

    z

--- pages/_documents.js
+++ pages/_documents.js
@@ -1,44 +1,35 @@
-import React from "react";
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { HTML, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
-class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+
+export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    // Step 1: Create an instance of ServerStyleSheet
     const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        });
+    // Step 2: Retrieve styles from components in the page
+    const page = renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />)
+    );
 
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      };
-    } finally {
-      sheet.seal();
-    }
+    // Step 3: Extract the styles as <style> tags
+    const styleTags = sheet.getStyleElement();
+
+    // Step 4: Pass styleTags as a prop
+    return { ...page, styleTags };
   }
 
   render() {
     return (
-      <Html lang="ko">
-        <Head />
+      <HTML>
+        <Head>
+          {/* Step 5: Output the styles in the head  */}
+          {this.props.styleTags}
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
-      </Html>
+      </HTML>
     );
   }
 }
-
-export default MyDocument;
